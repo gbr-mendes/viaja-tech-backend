ce6414f547da2c9ce0068d65d8242387d5a97ae9
Gabriel <gabrielx165@gmail.com>
2022-07-15 19:10:15 -0300

    removed unecessary function

--- src/utils/manageRoles.js
+++ src/utils/manageRoles.js
@@ -1,6 +1,5 @@
 const UserModel = require('../models/userSchema')
 const LeadModel = require('../models/leadSchema')
-const EmployeeModel = require('../models/employeeSchema')
 
 const utils = {}
 
@@ -11,44 +10,6 @@ utils.createLead = async (userId) => {
         console.log(err)
         await UserModel.findByIdAndDelete(userId)
     }
-}
-
-const createEmployee = async (userId, roles, data) => {
-    const employeeData = {}
-    const { salary } = data
-    employeeData.user = userId
-    employeeData.salary = salary
-
-    if (roles.includes('isAdmin')) {
-        employeeData.position = 'General Manager'
-    } else if (roles.includes('isSiteAdmin')) {
-        employeeData.position = 'Content Manager'
-    } else if (roles.includes('isSalesManager')) {
-        employeeData.position = 'Sales Manager'
-    }
-
-    try {
-        await EmployeeModel.create(employeeData)
-    } catch (err) {
-        console.log(err)
-        await UserModel.findByIdAndDelete(userId)
-    }
-}
-
-utils.setUserRole = (userId, data) => {
-    const { roles } = data
-
-    // All users created are also leads
-    createLead(userId)
-
-    if (
-        roles.includes('isSiteAdmin') ||
-        roles.includes('isSalesManager') ||
-        roles.includes('isAdmin')
-    ) {
-        createEmployee(userId, roles, data)
-    }
-
 }
 
 module.exports = utils
