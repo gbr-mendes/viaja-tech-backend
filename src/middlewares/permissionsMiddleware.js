6780cf6a5158ec11923f81e8638b9c2fa1ba3e0a
Gabriel <gabrielx165@gmail.com>
2022-07-16 13:04:07 -0300

    fixed bug on get token function

--- src/middlewares/permissionsMiddleware.js
+++ src/middlewares/permissionsMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const { getToken } = require('../utils/users')
 const middlewares = {}
 
 const checkRole = (roles, userRole) => {
@@ -11,12 +12,11 @@ const checkRole = (roles, userRole) => {
 
 middlewares.verifyPermission = (roles) => {
     return (req, resp, next) => {
-        const { authorization } = req.headers
-        const token = authorization.split(" ")[1]
+        const token = getToken(req)
 
         jwt.verify(token, process.env.TOKEN_SECRET, async function (err, decoded) {
             if (err) {
-                return resp.status(401).json({ error: 'Token inválido.' });
+                return resp.status(401).json({ error: 'Token inválido ou não informado.' });
             }
             const { role: userRole } = decoded
             const allowed = checkRole(roles, userRole)
