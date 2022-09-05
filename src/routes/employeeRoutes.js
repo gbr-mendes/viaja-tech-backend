dfaf5e55c2b7df5ffc029fca66c9a432f03347bf
Gabriel Mendes <gabrielx165@gmail.com>
2022-09-05 11:03:21 -0300

    add authentication and permission verification on package controller

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -1,16 +1,31 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // controller
-const controller = require('../controllers/employeeController')
+const controller = require("../controllers/employeeController");
 
 // middlewares
-const { authenticationRequired } = require('../middlewares/authMiddlewares')
-const { verifyPermission } = require('../middlewares/permissionsMiddleware')
+const { authenticationRequired } = require("../middlewares/authMiddlewares");
+const { verifyPermission } = require("../middlewares/permissionsMiddleware");
 
 // routes setup
-router.post('/', authenticationRequired, verifyPermission(["isAdmin"]), controller.createEmployee)
-router.get('/', authenticationRequired, verifyPermission(["isAdmin"]), controller.getEmployees)
-router.get('/:employeeId', authenticationRequired, verifyPermission(["isAdmin"]), controller.getEmployeeById)
+router.post(
+  "/",
+  authenticationRequired,
+  verifyPermission(["isAdmin"]),
+  controller.createEmployee
+);
+router.get(
+  "/",
+  authenticationRequired,
+  verifyPermission(["isAdmin"]),
+  controller.getEmployees
+);
+router.get(
+  "/:employeeId",
+  authenticationRequired,
+  verifyPermission(["isAdmin"]),
+  controller.getEmployeeById
+);
 
-module.exports = router
+module.exports = router;
--- src/routes/packageRoutes.js
+++ src/routes/packageRoutes.js
@@ -2,10 +2,33 @@ const express = require("express");
 const router = express.Router();
 const packageController = require("../controllers/packageController");
 
+// middlewares
+const { authenticationRequired } = require("../middlewares/authMiddlewares");
+const { verifyPermission } = require("../middlewares/permissionsMiddleware");
+
 router.get("/", packageController.getPackages);
+
 router.get("/:packageId", packageController.getPackageById);
-router.post("/", packageController.createPackage);
-router.patch("/:packageId", packageController.updatePackage);
-router.delete("/:packageId", packageController.deletePackage);
+
+router.post(
+  "/",
+  authenticationRequired,
+  verifyPermission(["isAdmin"]),
+  packageController.createPackage
+);
+
+router.patch(
+  "/:packageId",
+  authenticationRequired,
+  verifyPermission(["isAdmin"]),
+  packageController.updatePackage
+);
+
+router.delete(
+  "/:packageId",
+  authenticationRequired,
+  verifyPermission(["isAdmin"]),
+  packageController.deletePackage
+);
 
 module.exports = router;
